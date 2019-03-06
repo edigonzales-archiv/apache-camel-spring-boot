befc12cf7edec2302703b151add7ce7904117a01
Stefan Ziegler <edi.gonzales@gmail.com>
2019-03-06 21:25:49 +0100

    wip

--- src/main/java/ch/so/agi/apachecamel/MyRoute.java
+++ src/main/java/ch/so/agi/apachecamel/MyRoute.java
@@ -26,19 +26,23 @@ public class MyRoute extends RouteBuilder {
         // TODO:
         // - download only 'VOLLZUG' (?)
         
-        from("ftp://{{env:ftpUserInfogrips}}@ftp.infogrips.ch/\\gb2av\\?password={{env:ftpPwdInfogrips}}&antInclude=VOLLZUG*.zip&autoCreate=false&noop=true&stepwise=false&separator=Windows&passiveMode=true&binary=true&delay=5000&initialDelay=10000&idempotentRepository=#fileConsumerRepo&idempotentKey=${file:name}-${file:size}")
+        from("ftp://{{env:ftpUserInfogrips}}@ftp.infogrips.ch/\\gb2av\\?fileName=VOLLZUG_SO0200002401_1531_20170420081516.zip&password={{env:ftpPwdInfogrips}}&antInclude=VOLLZUG*.zip&autoCreate=false&noop=true&stepwise=false&separator=Windows&passiveMode=true&binary=true&delay=5000&initialDelay=10000&idempotentRepository=#fileConsumerRepo&idempotentKey=${file:name}-${file:size}")
+//        from("ftp://{{env:ftpUserInfogrips}}@ftp.infogrips.ch/\\gb2av\\?password={{env:ftpPwdInfogrips}}&antInclude=VOLLZUG*.zip&autoCreate=false&noop=true&stepwise=false&separator=Windows&passiveMode=true&binary=true&delay=5000&initialDelay=10000&idempotentRepository=#fileConsumerRepo&idempotentKey=${file:name}-${file:size}")
 //        from("ftp://{{env:ftpUserInfogrips}}@ftp.infogrips.ch/\\dm01avso24lv95_2\\shp\\?password={{env:ftpPwdInfogrips}}&autoCreate=false&noop=true&stepwise=false&separator=Windows&passiveMode=true&binary=true&delay=5000&initialDelay=10000&idempotentRepository=#fileConsumerRepo&idempotentKey=${file:name}-${file:size}")
 //        from("ftp://{{env:ftpUserInfogrips}}@ftp.infogrips.ch/\\dm01avso24lv95_2\\shp\\?password={{env:ftpPwdInfogrips}}&autoCreate=false&noop=true&stepwise=false&separator=Windows&passiveMode=true&binary=true&delay=5000&initialDelay=10000")
         .to("file:///Users/stefan/Downloads/output/")
         .split(new ZipSplitter())
-        .streaming().convertBodyTo(String.class) 
+        .streaming().convertBodyTo(String.class) //org.apache.camel.InvalidPayloadException: No body available of type: java.io.File but has value: org.apache.camel.dataformat.zipfile.ZipInputStreamWrapper@14fa8459 of type: org.apache.camel.dataformat.zipfile.ZipInputStreamWrapper on: Message[]. Caused by: No type converter available to convert from type: org.apache.camel.dataformat.zipfile.ZipInputStreamWrapper to the required type: java.io.File with value org.apache.camel.dataformat.zipfile.ZipInputStreamWrapper@14fa8459. Exchange[ID-SemucChampey-local-1551902511781-0-3]. Caused by: [org.apache.camel.NoTypeConversionAvailableException - No type converter available to convert from type: org.apache.camel.dataformat.zipfile.ZipInputStreamWrapper to the required type: java.io.File with value org.apache.camel.dataformat.zipfile.ZipInputStreamWrapper@14fa8459]
             .choice()
                 .when(body().isNotNull())
                     .to("file:///Users/stefan/Downloads/output_unzipped/")
             .end()
         .end();
-
-//        .to("ili2pg://disableValiation=true");
+        
+        from("file:///Users/stefan/Downloads/output_unzipped/")
+        .to("file:///Users/stefan/Downloads/output_unzipped_ready/")
+        .end();
+        
 //        .to("ili2pg://bar");
         
     }
